475366dd00b5841af370e1520073d98a1087a9bd
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-02-05 17:13:23 +0530

    Hello world

---
+++ hello.java
@@ -0,0 +1,13 @@
+package coding;
+import java.util.*;
+
+public class hello {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Hello world");
+		System.out.println(n+2);
+	}
+
+}
