dd757eed12e408970ac15a517f093b89b2629d3b
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-02-27 16:17:16 +0530

    Merge two sorted LL

---
+++ merge_two_sorted_ll.java
@@ -0,0 +1,22 @@
+package coding;
+
+public class merge_two_sorted_ll {
+	
+	public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
+	
+public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+    }
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
