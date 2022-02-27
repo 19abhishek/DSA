4c864ca9062a8254ab1dd065777ccb6fbe388f18
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-02-27 12:32:12 +0530

    Middle of linked list

---
+++ middle_of_linked_list.java
@@ -0,0 +1,23 @@
+package coding;
+
+
+public class middle_of_linked_list {
+	
+	public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
+	
+public ListNode middleNode(ListNode head) {
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
