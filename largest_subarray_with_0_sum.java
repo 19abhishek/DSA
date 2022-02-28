693d63f94cbf87da43ba36867e0618f4ef7307a2
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-02-28 21:56:30 +0530

    Largest subarray with 0 sum

--- largest_subarray_with_0_sum.java
+++ largest_subarray_with_0_sum.java
@@ -1,9 +1,33 @@
 package coding;
-
+import java.util.*;
 public class largest_subarray_with_0_sum {
+	
+	public int maxLen(int arr[], int n)
+    {
+		HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
+        int maxi = 0;
+        int sum = 0; 
+        for(int i = 0;i<n;i++) {
+            sum += arr[i]; 
+            if(sum == 0) {
+                maxi = i + 1; 
+            }
+            else {
+                if(mpp.get(sum) != null) {
+
+                    maxi = Math.max(maxi, i - mpp.get(sum)); 
+                }
+                else {
+
+                    mpp.put(sum, i); 
+                }
+            }
+        }
+        return maxi;
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
