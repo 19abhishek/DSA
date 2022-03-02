b9ef4b2298808f41f6c6feb3bcc7f55173e08914
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-03-02 22:07:41 +0530

    Unique Paths

--- unique_paths.java
+++ unique_paths.java
@@ -1,9 +1,23 @@
 package coding;
 
 public class unique_paths {
+	
+	public int uniquePaths(int m, int n) {
+        int[][] ans = new int[m][n];
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(i==0 || j==0){
+                    ans[i][j] = 1;
+                }else {
+                    ans[i][j] = ans[i-1][j]+ans[i][j-1];
+                }
+            }
+        }
+        return ans[m-1][n-1];
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
